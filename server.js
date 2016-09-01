dd6a7fd49690ebf3989e36561acc95b232559fb1
Muyiwa Akin-Ogundeji <oakinogundeji@gmail.com>
2016-09-01 16:19:32 +0100

    removed cluster module due to bugs

--- server.js
+++ server.js
@@ -5,8 +5,13 @@
 //=============================================================================
 const
   http = require('http'),
-  cluster = require('cluster'),
   app = require('./app');
+//=============================================================================
+/**
+ * Create Server instance
+ */
+//=============================================================================
+const server = http.createServer(app);
 //=============================================================================
 /**
  * Module variables
@@ -17,40 +22,12 @@ const
   env = app.get('env');
 //=============================================================================
 /**
- * Check if instance is master
+ * Bind server to port
  */
 //=============================================================================
-if(cluster.isMaster) {
-    const CPUNUMs = require('os').cpus().length;
-    let i;
-    for(i = 0; i < CPUNUMs; i++) {
-        console.log('Now forking worker number %d', i);
-        cluster.fork();
-        }
-    cluster.on('online', worker => console.log('Worker %s is online', worker.process.pid));
-    cluster.on('exit', (worker, code, signal) => {
-        console.log('Worker %s died with code: %s and signal: %s', worker.process.pid, code, signal);
-        console.log('Starting a new worker');
-        setTimeout(() => {
-            return cluster.fork();
-            }, 1000 * 5);
-        });
-}
-//=============================================================================
-else {
-    /**
-     * Create server instance
-     */
-    //=========================================================================
-    const server = http.createServer(app);
-    //========================================================================
-    /**
-     * Bind server to port
-     */
-    //=========================================================================
-    server.listen(port, () => console.log('Textpedia server listening on port %d in %s mode', port, env));
-    //=========================================================================
-}
+server.listen(port, function () {
+  return console.log('Textpedia server listening on port %d in %s mode', port, env);
+});
 //=============================================================================
 /**
  * Conditionally export module
